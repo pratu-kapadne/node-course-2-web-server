2a257a94a335ea654025444879e7f14a7e06410c
pratiksha kapadne <pratikshakapadne52@gmail.com>
2018-09-28 21:02:23 +0530

    Add project page

--- server.js
+++ server.js
@@ -44,6 +44,13 @@ app.get('/', (req, res)   => {
   });
 });
 
+app.get('/projects',(req,res) =>{
+  res.render('projects.hbs',{
+    pageTitle:'projects'
+
+  });
+});
+
 app.get('/about', (req,res)=>{
 res.render('about.hbs', {
   pageTitle: ' About page'
