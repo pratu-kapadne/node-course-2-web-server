ab3eaf47173df7d60ba90dc55a6741babecfedaa
pratiksha kapadne <pratikshakapadne52@gmail.com>
2018-09-28 19:25:40 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs= require('fs');
 
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -15,11 +15,12 @@ app.use((req,res,next)=> {
 var log = `${now}:${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log',log + '\n', (err)=>{
-    if(err){
-      console.log('Unable to append to server.log')
-    }
-  });
+  fs.appendFile('server.log',log + '\n');
+  // , (err)=>{
+  //   if(err){
+  //     console.log('Unable to append to server.log')
+  //   }
+  // });
   next();
 });
 
@@ -56,7 +57,6 @@ app.get('/bad', (req,res) =>{
   });
 });
 
-
-app.listen(3000), () => {
-  console.log('Server is up on port 3000');
-};
+app.listen(port,() => {
+  console.log(`Server is up on port  ${port}`);
+});
